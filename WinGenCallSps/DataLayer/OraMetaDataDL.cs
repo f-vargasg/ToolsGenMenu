48ddd297c9468a6cc3f4f4f21815e4487441debb
Francisco Vargas G <52474105+f-vargasg@users.noreply.github.com>
2020-06-03 22:06:30 -0600

    Correccion para que funcionara con .net 4.7.2

--- WinGenCallSps/DataLayer/OraMetaDataDL.cs
+++ WinGenCallSps/DataLayer/OraMetaDataDL.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using Utilities;
 
-namespace WinXmlToSqlInvoker.DataLayer
+namespace WinGenCallSps.DataLayer
 {
     public class OraMetaDataDL
     {
--- WinManteCatalogoServ/FrmListCatalog.Designer.cs
+++ WinManteCatalogoServ/FrmListCatalog.Designer.cs
@@ -28,7 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.buscarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buscarToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(110, 26);
+            // 
+            // buscarToolStripMenuItem
+            // 
+            this.buscarToolStripMenuItem.Name = "buscarToolStripMenuItem";
+            this.buscarToolStripMenuItem.Size = new System.Drawing.Size(109, 22);
+            this.buscarToolStripMenuItem.Text = "Buscar";
             // 
             // FrmListCatalog
             // 
@@ -37,11 +54,15 @@
             this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
             this.Name = "FrmListCatalog";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem buscarToolStripMenuItem;
     }
 }
